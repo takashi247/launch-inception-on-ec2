10a1111eab1e39e7320863952862ed927be4dc16
takashi247 <takashi247@gmail.com>
2021-06-13 01:16:36 +0900

    modify files to connect nginx/wordpress with a container from official mariadb image

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -4,7 +4,8 @@ service mysql start
 
 mysql <<MYSQL_SCRIPT
 CREATE DATABASE wpdb;
-CREATE USER 'wpuser'@'localhost' identified by 'dbpassword';
+CREATE USER 'wpuser'@'localhost' IDENTIFIED BY 'dbpassword';
+GRANT ALL ON *.* TO 'root'@'mariadb' IDENTIFIED BY 'dbpassword' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON wpdb.* TO 'wpuser'@'localhost';
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
