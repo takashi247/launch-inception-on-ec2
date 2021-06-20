0c513bee86fabe3ad1d24a5e91b7012b2f9a3556
takashi247 <takashi247@gmail.com>
2021-06-20 12:55:28 +0900

    disable wp debug log

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -77,8 +77,8 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_LOG', true);
+// define( 'WP_DEBUG', true );
+// define( 'WP_DEBUG_LOG', true);
 
 /* That's all, stop editing! Happy publishing. */
 
