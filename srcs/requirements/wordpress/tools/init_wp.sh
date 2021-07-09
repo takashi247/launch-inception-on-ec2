fdef7005ec41048008962e4c7b1c19876d0b5285
takashi247 <takashi247@gmail.com>
2021-07-10 00:10:01 +0900

    modify wordpress shell script and dockerfile to install and set up wordpress under the name of www-data

--- srcs/requirements/wordpress/tools/init_wp.sh
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -22,18 +22,18 @@ if [ ! "$(ls -A /var/www/${DOMAIN_NAME})" ]; then
         sleep 1
     done
     # install wordpress
-    wp core install \
+    gosu www-data \
+        /usr/local/bin/wp core install \
         --url=${DOMAIN_NAME} \
         --title="${TITLE}" \
         --admin_user=${ADMIN_USER} \
         --admin_password=${ADMIN_PASSWORD} \
-        --admin_email=${ADMIN_EMAIL} \
-        --allow-root
+        --admin_email=${ADMIN_EMAIL}
     # create users
-    wp user create \
+    gosu www-data \
+        /usr/local/bin/wp user create \
         ${USER_LOGIN} \
         ${USER_EMAIL} \
         --role=${ROLE} \
-        --user_pass=${USER_PASSWORD} \
-        --allow-root
+        --user_pass=${USER_PASSWORD}
 fi
